7be9fcad56f6f398efbe28b4d5d225b0c3f2b9ba
Aniruddhaghosh27 <aniruddhaghosh507@gmail.com>
2026-03-16 18:52:31 +0600

    Calculation

--- Z_JS/Validation.js
+++ Z_JS/Validation.js
@@ -3,3 +3,13 @@ const days = 30;
 
 const quantityInput = document.getElementById("quantity");
 const totalPriceInput = document.getElementById("totalPrice");
+
+function calculateTotal(){
+
+let quantity = parseInt(quantityInput.value) || 0;
+
+if(quantity < 0){
+alert("Quantity cannot be negative. Resetting to 0.");
+quantity = 0;
+quantityInput.value = 0;
+}
